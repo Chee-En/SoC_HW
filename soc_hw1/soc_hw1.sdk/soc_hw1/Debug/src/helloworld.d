3a19e63c03cb8c1170047d8faa30f3bf1d0f37ee
Chee-En <90122434+Chee-En@users.noreply.github.com>
2025-04-09 16:54:23 +0800

    Homework1

---
+++ soc_hw1/soc_hw1.sdk/soc_hw1/Debug/src/helloworld.d
@@ -0,0 +1,43 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
---
+++ soc_hw1/soc_hw1.sdk/soc_hw1/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../soc_hw1_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../soc_hw1_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
